d0990a4fc539f8828b0425b8e41280c7fda3f204
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2019-05-12 04:25:19 +0700

    Resolving new dependencies

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'css/index.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import App from 'App';
 import registerServiceWorker from 'registerServiceWorker';
 
 ReactDOM.render((
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 ), document.getElementById('root'));
 registerServiceWorker();
