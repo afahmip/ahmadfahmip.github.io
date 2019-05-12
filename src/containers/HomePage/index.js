1f23340f028158e67a8c1fc4c4829d96ac031a99
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2019-05-12 15:01:48 +0700

    Add media queries on intro

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -7,17 +7,22 @@ class HomePage extends Component {
     return (
       <div id="home">
         <div className="intro page-container">
-          <h1>frontend + ui/ux <span>enthusiast</span></h1>
+          <h1>frontend & ui/ux <span>enthusiast</span></h1>
           <h1>self-claimed <i>hackathlete</i></h1>
-          <h1>based in Bandung, ID</h1>
-          <p className="short-desc">Passion-driven self-taught engineer. For me, everything is an art. It's a masterpiece that must be carefully crafted.</p>
-          <button className="medium row">
-            <i className="fab fa-medium"></i>
-            <p>&nbsp;MY LATEST ARTICLE |&nbsp;</p>
-            <p>PORTFOLIO: REDESIGNED</p>
+          <h1 className="row">based in Bandung, ID_</h1>
+          {/* <div className="tail" /> */}
+          <p className="short-desc">Passion-driven self-taught engineer. For me, everything is an art.<br/>It's a masterpiece that must be carefully crafted.</p>
+          <button className="medium">
+            <div className="row">
+              <i className="fab fa-medium"></i>
+              &nbsp;
+              <p>LATEST ARTICLE |&nbsp;</p>
+              <p>HOW I GOT MY FIRST INTERNSHIP ON GOOGLE</p>
+            </div>
+            <div className="over" />
           </button>
           <div className="portfolio">
-            
+            {/* <h1>PORTFOLIO</h1> */}
           </div>
         </div>
       </div>
