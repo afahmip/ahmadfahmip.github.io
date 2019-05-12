b938b9fd16d5f630c54b7c74791b1ec2ba8c871a
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2019-05-12 16:56:41 +0700

    Add medium url fetch

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -3,7 +3,40 @@ import { Link } from 'react-router-dom'
 import './index.scss'
 
 class HomePage extends Component {
-  render() {
+  state = {
+    medium: {
+      title: '...',
+      url: ''
+    }
+  }
+
+  fetchMedium () {
+    fetch(`https://cors.io/?https://medium.com/@afahmip/latest?format=json`)
+      .then(response => response.text())
+      .then(response => {
+        let data = response.replace('])}while(1);</x>', '')
+        data = JSON.parse(data)
+        let posts = data.payload.references.Post
+        let keys = Object.keys(posts)
+        this.setMedium(posts[keys[0]], keys[0])
+      })
+      .catch(error => console.error(error))
+  }
+
+  setMedium (post, id) {
+    this.setState({
+      medium: {
+        title: post.title.toUpperCase(),
+        url: `https://medium.com/p/${id}`
+      }
+    })
+  }
+
+  componentDidMount () {
+    this.fetchMedium()
+  }
+
+  render () {
     return (
       <div id="home">
         <div className="intro page-container">
@@ -12,15 +45,17 @@ class HomePage extends Component {
           <h1 className="row">based in Bandung, ID_</h1>
           {/* <div className="tail" /> */}
           <p className="short-desc">Passion-driven self-taught engineer. For me, everything is an art.<br/>It's a masterpiece that must be carefully crafted.</p>
-          <button className="medium">
-            <div className="row">
-              <i className="fab fa-medium"></i>
-              &nbsp;
-              <p>LATEST ARTICLE |&nbsp;</p>
-              <p>HOW I GOT MY FIRST INTERNSHIP ON GOOGLE</p>
-            </div>
-            <div className="over" />
-          </button>
+          <a href={ this.state.medium.url } target="_blank">
+            <button className="medium">
+              <div className="row">
+                <i className="fab fa-medium"></i>
+                &nbsp;
+                <p>LATEST ARTICLE |&nbsp;</p>
+                <p>{ this.state.medium.title }</p>
+              </div>
+              <div className="over" />
+            </button>
+          </a>
           <div className="portfolio">
             {/* <h1>PORTFOLIO</h1> */}
           </div>
