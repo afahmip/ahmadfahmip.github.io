8db836e6aad614346e0a96dfdfbb89aaa4abe911
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2019-05-24 23:15:57 +0700

    Updates

---
+++ precache-manifest.9eaffc61a2ba6e71f2805baab4436efc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6364499822d14b2dca61",
+    "url": "/static/js/main.319eba5d.chunk.js"
+  },
+  {
+    "revision": "326dba7a7a0c6d450d65",
+    "url": "/static/js/2.a5cc4420.chunk.js"
+  },
+  {
+    "revision": "6364499822d14b2dca61",
+    "url": "/static/css/main.e0363eac.chunk.css"
+  },
+  {
+    "revision": "27574e0283ec4ed24e07a3dacdf26795",
+    "url": "/index.html"
+  }
+];
